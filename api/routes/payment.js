e3479385d13891f5b2b6ba40e219e7c8a471bff5
Harmaton <njagiiharmaton@gmail.com>
2022-12-09 17:57:08 +0300

    m-pesa api

---
+++ api/routes/payment.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+const axios = require('axios');
+
+ //generate token middleware
+ const generatetoken = async (req, res, next) => {
+    const secret = process.env.MPESA_CONSUMER_SECRET_KEY
+    const consumer = process.env.MPESA_CONSUMER
+    const auth = new Buffer.from(`${consumer}: ${secret}`).toString("base64")
+
+        await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+         {
+            headers : {
+                authorization: `Basic ${auth}`
+            }
+        }).then((response) => {
+            // console.log(data);
+            token = response.data.access_token;
+            next();
+        }).catch((err) => {
+            console.log(err);
+            res.status(400).json(err.message)
+        })   
+ }
+
+ router.get("/token", (res,req) => {
+    generatetoken()
+ })
+
+//MPESA PAYMENTS API
+router.post("/stk", generatetoken, async (res, req) => {
+    const phone = req.body.phone.subString[0];
+    const amount = req.body.amount;
+     
+    const date = new Date();
+
+    const timestamp = 
+    date.getFullYear()+ 
+    ("0" + (date.getMonth() + 1)).slice(-2)+
+    ("0" + date.getDate()).slice(-2)+
+    ("0" + date.getHours()).slice(-2)+
+    ("0" + date.getMinutes()).slice(-2)+
+    ("0" + date.getSeconds()).slice(-2);
+
+    const shortCode = process.env.MPESA_PAYBILL;
+    const passKey = process.env.MPESA_PASSKEY;
+
+    const password = new Buffer.from(shortCode + passKey+ timestamp).toString("base64")
+    
+
+   
+    await axios.post("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+    {    
+        "BusinessShortCode":shortCode,    
+        "Password": password,    
+      "Timestamp":timestamp,    
+      "TransactionType": "CustomerPayBillOnline",    
+        "Amount":amount,    
+       "PartyA":`254${phone}`,    
+        "PartyB":shortCode,    
+      "PhoneNumber":`254${phone}`,    
+      "CallBackURL":"https://mydomain.com/pat",    
+      "AccountReference": `254${phone}`,    
+      "TransactionDesc":"Test"
+     },
+     {
+        header: {
+            Authorization: `Bearer ${token}`, 
+        }
+     }
+
+    ).then( (data) => {
+        try{
+        console.log(data.data);
+        res.status(200).json(data.data)
+        }catch(err){
+            console.log(err);
+            res.status(400).json(err.message)
+        }
+    })
+})
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 
 
 const mongoose = require("mongoose");
@@ -13,6 +13,7 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const orderRoute = require('./routes/order')
 const cartRoute = require('./routes/cart')
+const paymentRoute = require('./routes/payment')
 
 dotenv.config();
 
@@ -20,12 +21,18 @@ mongoose.connect(process.env.MONGO).then(() => console.log("DB connection succes
     console.log(err);
 })
 
+
+
 //End-Points
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
 app.use("/api ", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute)
-app.use('api/orders', orderRoute)
+app.use('/api/orders', orderRoute)
 app.use('/api/cart', cartRoute)
+app.use('/api/payment', paymentRoute)
 
 app.listen(process.env.PORT || 5000 , () => console.log("server running!!"));
+
